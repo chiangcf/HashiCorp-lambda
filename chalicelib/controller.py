4b71be3a0f7fe1e9c44f12ba077e666f87498db5
Christian <christiandcf@gmail.com>
2018-11-27 15:16:32 -0600

    clean up

--- chalicelib/controller.py
+++ chalicelib/controller.py
@@ -4,12 +4,19 @@ import requests
 
 
 # This will make a HTTP request to the server containing the vault and secrets
+from chalice import BadRequestError
+
+
 def vault_controller(request, token):
-    end_point = request['endpoint']
-    url = "http://34.207.239.44/{}".format(end_point)
+    end_point = request['secret']
+    url = "http://34.207.239.44/secrets/{}".format(end_point)
     headers = {
         'Content-type': "application/json",
         'X-Vault-Token': token
     }
-    body = requests.get(url, headers=headers)
+    try:
+        body = requests.get(url, headers=headers)
+    except Exception as e:
+        raise BadRequestError(e)
+
     return json.loads(body.content)
